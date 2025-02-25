eb326b1b177ebbdd038554c520924af3abdad976
Colm Seale <c.f.seale@tudelft.nl>
2025-02-25 16:31:04 +0100

    bug fixes

--- slurm/XCRISP/prepare.sh
+++ slurm/XCRISP/prepare.sh
@@ -1,19 +1,19 @@
 # FORECasT data
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch train
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch test
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch train
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch test
 
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch HAP1_train
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch HAP1_test
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch HAP1_train
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch HAP1_test
 
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch TREX_A_test
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch TREX_A_train
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch TREX_A_test
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch TREX_A_train
 
 
 # inDelphi Data
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch 0105-mESC-Lib1-Cas9-Tol2-BioRep2-techrep1
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0105-mESC-Lib1-Cas9-Tol2-BioRep2-techrep1
 
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertrain
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertest
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertrain
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertest
 
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertrain
-sbatch $PROTONDDR/repos/X-CRISP/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertest
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertrain
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertest
