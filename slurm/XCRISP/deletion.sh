a01c0f79828ff18d2f14557f4d6adb82c0e172c5
Colm Seale <c.f.seale@tudelft.nl>
2025-02-25 18:33:56 +0100

    bug fixes

--- slurm/XCRISP/deletion.sh
+++ slurm/XCRISP/deletion.sh
@@ -1,6 +1,6 @@
 
-for LEARNING_RATE in 0.1 0.05 0.01 0.001
-    do for LOSS in kld mse 
+for LEARNING_RATE in 0.1 0.05 0.01 0.001;
+    do for LOSS in kld mse; 
         do
             sbatch deletion.batch $LEARNING_RATE $LOSS
         done
