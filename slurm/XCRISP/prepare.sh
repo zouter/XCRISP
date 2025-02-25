ae5f360897c384b17d3e14d99a735900857680cd
Colm Seale <c.f.seale@tudelft.nl>
2025-02-25 17:02:00 +0100

    bug fixes

--- slurm/XCRISP/prepare.sh
+++ slurm/XCRISP/prepare.sh
@@ -10,10 +10,10 @@ sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch TREX_A_train
 
 
 # inDelphi Data
-sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0105-mESC-Lib1-Cas9-Tol2-BioRep2-techrep1
+# sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0105-mESC-Lib1-Cas9-Tol2-BioRep2-techrep1
 
-sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertrain
-sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertest
+# sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertrain
+# sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 0226-PRLmESC-Lib1-Cas9_transfertest
 
-sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertrain
-sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertest
+# sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertrain
+# sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/prepare.batch 052218-U2OS-+-LibA-postCas9-rep1_transfertest
