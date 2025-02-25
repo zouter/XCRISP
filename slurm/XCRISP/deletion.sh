4836fb615ff992ca83a8b3f99b27ffd607375c4c
Colm Seale <c.f.seale@tudelft.nl>
2025-02-25 18:35:27 +0100

    bug fixes

--- slurm/XCRISP/deletion.sh
+++ slurm/XCRISP/deletion.sh
@@ -2,7 +2,7 @@
 for LEARNING_RATE in 0.1 0.05 0.01 0.001;
     do for LOSS in kld mse; 
         do
-            sbatch deletion.batch $LEARNING_RATE $LOSS
+            sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/deletion.batch $LEARNING_RATE $LOSS
         done
     done
 done
