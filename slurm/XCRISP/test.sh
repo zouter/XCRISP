93ac79c74556c36492f18dd8de523d230fd0a29a
Colm Seale <c.f.seale@tudelft.nl>
2025-02-28 17:37:12 +0100

    rerunning tests

--- slurm/XCRISP/test.sh
+++ slurm/XCRISP/test.sh
@@ -1,2 +1,2 @@
-sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/test.batch mse 0.05
-sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/test.batch kld 0.1
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/test.batch mse 0.01
+sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/test.batch kld 0.05
