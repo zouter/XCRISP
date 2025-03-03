36337dd2ff731a69cdb4fa89a7a41cd7d068f8f1
Colm Seale <c.f.seale@tudelft.nl>
2025-03-03 15:56:23 +0100

    fixed transfer learning scripts

--- slurm/Lindel/LR_insertion_transfer.sh
+++ slurm/Lindel/LR_insertion_transfer.sh
@@ -1,5 +1,5 @@
 for GENOTYPE in 0226-PRLmESC-Lib1-Cas9_transfertrain 052218-U2OS-+-LibA-postCas9-rep1_transfertrain HAP1_train TREX_A_train
 do 
-    sbatch LR_insertion.batch $GENOTYPE baseline
-    sbatch LR_insertion.batch $GENOTYPE pretrained 
+    sbatch $PROTONDDR/repos/x-crisp/slurm/Lindel/LR_insertion.batch $GENOTYPE baseline
+    sbatch $PROTONDDR/repos/x-crisp/slurm/Lindel/LR_insertion.batch $GENOTYPE pretrained 
 done
--- slurm/XCRISP/transfer.sh
+++ slurm/XCRISP/transfer.sh
@@ -6,10 +6,10 @@
 for MODE in pretrained baseline pretrainedsamearch pretrainedplusonelayer pretrainedonefrozenlayer weightinit
     do for NUM_SAMPLES in 2 5 10 20 50 100 200 500 
         do 
-        sbatch transfer.batch $MODE $NUM_SAMPLES 0226-PRLmESC-Lib1-Cas9_transfertrain mESC-NHEJ-deficient
-        sbatch transfer.batch $MODE $NUM_SAMPLES 052218-U2OS-+-LibA-postCas9-rep1_transfertrain U2OS
-        sbatch transfer.batch $MODE $NUM_SAMPLES HAP1_train HAP1
-        sbatch transfer.batch $MODE $NUM_SAMPLES TREX_A_train TREX_A
+        sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/transfer.batch $MODE $NUM_SAMPLES 0226-PRLmESC-Lib1-Cas9_transfertrain mESC-NHEJ-deficient
+        sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/transfer.batch $MODE $NUM_SAMPLES 052218-U2OS-+-LibA-postCas9-rep1_transfertrain U2OS
+        sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/transfer.batch $MODE $NUM_SAMPLES HAP1_train HAP1
+        sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/transfer.batch $MODE $NUM_SAMPLES TREX_A_train TREX_A
     done
 done
 
