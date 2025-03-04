6a79c11f2586c503f1e3905bc85cfb213d796819
Colm Seale <c.f.seale@tudelft.nl>
2025-03-04 17:37:18 +0100

    tested transfer

--- slurm/XCRISP/test_transfer.sh
+++ slurm/XCRISP/test_transfer.sh
@@ -1,5 +1,4 @@
-# for MODE in pretrainedplusonelayer pretrainedonefrozenlayer weightinit pretrained baseline pretrainedsamearch 
-for MODE in baseline
+for MODE in pretrainedplusonelayer pretrainedonefrozenlayer weightinit pretrained baseline pretrainedsamearch 
     do for NUM_SAMPLES in 2 5 10 20 50 100 200 500   
         do sbatch $PROTONDDR/repos/x-crisp/slurm/XCRISP/test_transfer.batch $MODE $NUM_SAMPLES
     done
