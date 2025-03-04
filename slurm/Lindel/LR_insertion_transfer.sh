9903f039d73b2d3a18aa015dd5a94da80a893546
Colm Seale <c.f.seale@tudelft.nl>
2025-03-04 16:45:51 +0100

    bug fixing

--- slurm/Lindel/LR_insertion_transfer.sh
+++ slurm/Lindel/LR_insertion_transfer.sh
@@ -1,5 +1,5 @@
 for GENOTYPE in 0226-PRLmESC-Lib1-Cas9_transfertrain 052218-U2OS-+-LibA-postCas9-rep1_transfertrain HAP1_train TREX_A_train
 do 
-    # sbatch $PROTONDDR/repos/x-crisp/slurm/Lindel/LR_insertion_transfer.batch $GENOTYPE pretrained
+    sbatch $PROTONDDR/repos/x-crisp/slurm/Lindel/LR_insertion_transfer.batch $GENOTYPE pretrained
     sbatch $PROTONDDR/repos/x-crisp/slurm/Lindel/LR_insertion_transfer.batch $GENOTYPE baseline 
 done
